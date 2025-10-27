ff2a6dfd6be940fe6e30c269b4d2fd44fd923b1b
xinput <yuan.lai@hellogroup.com>
2025-10-28 00:00:31 +0800

    add: 20251015

--- stock/src/main/java/com/xinput/learn/stock/controller/StockController.java
+++ stock/src/main/java/com/xinput/learn/stock/controller/StockController.java
@@ -1,17 +1,24 @@
 package com.xinput.learn.stock.controller;
 
+import com.google.common.collect.Maps;
 import com.xinput.learn.stock.consts.StockCache;
 import com.xinput.learn.stock.model.Stock;
+import com.xinput.learn.stock.service.StockService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/stock")
 public class StockController {
+
+    @Resource
+    private StockService stockService;
 
     @GetMapping("/status")
     public String status() {
@@ -26,5 +33,23 @@ public class StockController {
     @GetMapping("/get/{code}")
     public Stock get(@PathVariable(name = "code") String code) {
         return StockCache.stockMap.get(code);
+    }
+
+    @GetMapping("/reason")
+    public Map<String, Object> reason() {
+        List<Stock> stocks = stockService.reason();
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("size", stocks.size());
+        map.put("stocks", stocks);
+        return map;
+    }
+
+    @GetMapping("/noreason")
+    public Map<String, Object> noreason() {
+        List<Stock> stocks = stockService.noreason();
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("size", stocks.size());
+        map.put("stocks", stocks);
+        return map;
     }
 }
--- stock/src/main/java/com/xinput/learn/stock/service/StockService.java
+++ stock/src/main/java/com/xinput/learn/stock/service/StockService.java
@@ -1,8 +1,33 @@
 package com.xinput.learn.stock.service;
 
+import com.google.common.collect.Lists;
+import com.xinput.learn.stock.consts.StockCache;
+import com.xinput.learn.stock.model.Stock;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StockService {
 
+    public List<Stock> reason() {
+        List<Stock> stocks = Lists.newArrayListWithCapacity(1024 * 6);
+        for (Stock stock : StockCache.stockMap.values()) {
+            if (CollectionUtils.isNotEmpty(stock.getReason())) {
+                stocks.add(stock);
+            }
+        }
+        return stocks;
+    }
+
+    public List<Stock> noreason() {
+        List<Stock> stocks = Lists.newArrayListWithCapacity(1024 * 6);
+        for (Stock stock : StockCache.stockMap.values()) {
+            if (CollectionUtils.isEmpty(stock.getReason())) {
+                stocks.add(stock);
+            }
+        }
+        return stocks;
+    }
 }
